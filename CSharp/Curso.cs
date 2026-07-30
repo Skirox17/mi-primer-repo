68dccf525d1204c020d57fd4cd86ec1b8756b3e2
Shiro Vargas <shiro.var17@gmail.com>
2026-07-29 19:39:12 -0500

    Se agrego el archivo curso.cs

---
+++ CSharp/Curso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMatricula
+{
+    public class Curso
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+
+        public Curso(string nombre, decimal precio)
+        {
+            Nombre = nombre;
+            Precio = precio;
+        }
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
+    }
+}
